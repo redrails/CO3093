74819151e1689fb8f0e3bf2eb6052e325cbc8b2d
Ihtasham <redrailz@gmail.com>
2018-02-24 23:33:08 +0000

    finished Q1

--- CW1/CW1_mic7/Question_1_mic7.py
+++ CW1/CW1_mic7/Question_1_mic7.py
@@ -5,31 +5,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from scipy.stats import poisson, skellam
-import matplotlib.mlab as mlab
-import math
 from collections import Counter
 
-
-# Then we want to read the data file into our program.
 
 data = pd.read_csv("resources/PremierLeague1718.csv") # Read the data set file
 
 
-# The data file consists of empty columns and rows which we want to clean up so that we are working with _clean_ data. To do this we can use the below function.
-
+# The data file consists of empty columns and rows which we want to clean up so that we are working with _clean_ data.
+# To do this we can use the below function.
 def clean(dataframe):
     assert isinstance(dataframe, pd.DataFrame), 'Argument of wrong type!'
     return dataframe.dropna(axis=1, how='all')
 
 
-# And then we can apply this to our data.
-
 data = clean(data) # Clean the data up to remove empty and NaN cols
 
 
-# Because the data file consists of a lot of columns, some of them are irrelevant to us, the below function can help eliminate these columns, to do this we can specify only the columns we want to keep as a list of column names.
-
+# Because the data file consists of a lot of columns, some of them are irrelevant to us,
+# the below function can help eliminate these columns, to do this we can specify only the columns we want to keep as a list of column names.
 def filterCols(dataframe, cols):
     assert isinstance(dataframe, pd.DataFrame), 'Argument of wrong type!'
     assert isinstance(cols, list), 'Argument of wrong type!'
@@ -37,58 +30,61 @@ def filterCols(dataframe, cols):
 
 
 # We can then use this function to filter our data.
-
 colsToKeep = ["Date", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "HS", "AS", "HST", "AST", "FTR"] # List of cols we want to keep
 data = filterCols(data, colsToKeep) # Filter relevant cols
 
 
-# To get an idea of how the data looks like, we can use the `describe` function to give us various information. (This is helpful to answer Question 1, part 1.)
-
+# To get an idea of how the data looks like, we can use the `describe` function to give us various information.
+# (This is helpful to answer Question 1, part 1.)
 print("Description:")
 print(data.describe())
 
 
-# Furthermore, we can also group the data in a way so that we can see an outlook of how many goals were scored and conceded by each team over the whole league.
-
-
+# This function will allow us to rename an existing column in a dataframe.
 def renameCols(df, colsdict):
+    assert isinstance(df, pd.core.series.Series), 'Argument of wrong type!'
+    assert isinstance(colsdict, dict), 'Argument of wrong type!'
+    assert len([x for x in colsdict.keys() if x in df]) != len(colsdict), "One or more columns you want to rename do not exist in the dataframe"
     return df.reset_index().rename(columns=colsdict)
 
+
+# Furthermore, we can also group the data in a way so that we can see an outlook of how many goals were scored
+# and conceded by each team over the whole league.
 def creategroups():
     groups = [0,0,0,0]
     groups[0] = renameCols(data.groupby("HomeTeam")["FTR"].apply(lambda x: x[x == 'H'].count()), {"HomeTeam": "Team", "FTR": "Wins at home"})
     groups[1] = renameCols(data.groupby("AwayTeam")["FTR"].apply(lambda x: x[x == 'A'].count()), {"AwayTeam": "Team", "FTR": "Wins away"})
     groups[2] = renameCols(data.groupby("HomeTeam")["FTR"].apply(lambda x: x[x == 'A'].count()), {"HomeTeam": "Team", "FTR": "Losses at home"})
     groups[3] = renameCols(data.groupby("AwayTeam")["FTR"].apply(lambda x: x[x == 'H'].count()), {"AwayTeam": "Team", "FTR": "Losses away"})
-
     return groups
 
+# Then we can merge these groups to present the data well.
 groups = creategroups()
 merged = groups[0].merge(groups[1], on="Team").merge(groups[2], on="Team").merge(groups[3], on="Team")
 
+# After this we can see an outlook on wins and losses for all teams and analyse the data which is also helpful for Q1.1.
 print("Data on wins/losses")
 print(merged)
 
+# We can also see the results on averages from the describe page
 print("Averages of the wins/losses")
 print(merged.describe())
 
 
-# We can create a data structure containing information about the teams we care about. In this instance Manchester United and Manchester City. This can help us answer Question 1 part 2.
-
+# We can create a data structure containing information about the teams we care about. In this instance Manchester United and Manchester City.
+# This can help us answer Question 1 part 2.
 rteams = ["Man United", "Man City"] # Teams we care about
 
+# Creating a dictionary to store data for the relevant teams
 teams = {"ManUnitedHome": "", "ManCityHome": "", "ManUnitedAway": "", "ManCityAway": ""} # A dict to contain relevant team dataframes
 
+# Populate the data into the respective teams
 teams["ManUnitedHome"] = data.loc[data["HomeTeam"] == "Man United"]
 teams["ManUnitedAway"] = data.loc[data["AwayTeam"] == "Man United"]
 teams["ManCityHome"] = data.loc[data["HomeTeam"] == "Man City"]
 teams["ManCityAway"] = data.loc[data["AwayTeam"] == "Man City"]
 
-def mergeteamdata():
-    df = teams["ManUnitedHome"].append(teams["ManUnitedAway"])
-    df = df.append((teams["ManCityHome"].append(teams["ManCityAway"])))
-    return df
-
+# Printing data for each team playing both home and away respectively.
 print("Man United Home:")
 print(teams["ManUnitedHome"].describe())
 print("\nMan City Home")
@@ -100,9 +96,11 @@ print("\nMan City Away")
 print(teams["ManCityAway"].describe())
 
 
-# To get a better visualisation of how these teams stack up against eachother we can plot these values onto a graph t compare Manchester United's home defence against Manchester City's away offence and Manchester City's home defence against Manchester United's away offence.
-
+# To get a better visualisation of how these teams stack up against eachother we can plot these values onto a graph to
+# compare Manchester United's home defence against Manchester City's away offence and Manchester City's home defence
+# against Manchester United's away offence.
 def plotstats(stat):
+    assert isinstance(stat, list), 'Argument of wrong type!'
     fig = plt.figure(1)
     fig.set_size_inches(12, 8)
     for i in stat:
@@ -119,8 +117,8 @@ def plotstats(stat):
     plt.show()
 
 
-# We can create a simple object to use with the above function because it gives us a dynamic way of displaying information for different teams. 
-
+# We can create a simple object to use with the above function because it gives us a dynamic way of displaying information for different teams.
+# This is useful in plotting for multiple graphs in one go.
 class PlotObject:
     def __init__(self, df, col, glabel, xlabel, ylabel, plots):
         self.df     = df
@@ -131,20 +129,15 @@ class PlotObject:
         self.plots  = plots
 
 
-# We can then plot these teams and see how the data looks like. This will help answer Question 1 part 3. 
-
-# ### Manchester United's home defence vs Manchester City's away offence
-
-
+# We can then plot these teams and see how the data looks like. This will help answer Question 1 part 3.
+# Manchester United's home defence vs Manchester City's away offence
 obj = []
 obj.append(PlotObject(teams["ManUnitedHome"], "FTAG", "Man Utd Home", "Number of goals conceded", "Number of Matches", [221,223]))
 obj.append(PlotObject(teams["ManCityAway"], "FTAG", "Man City Away", "Number of goals scored", "Number of Matches", [222,224]))
 plotstats(obj)
 
 
-# ### Manchester City's home defence vs Manchester United's away offence
-
-
+# Manchester City's home defence vs Manchester United's away offence
 obj = []
 obj.append(PlotObject(teams["ManCityHome"], "FTAG", "Man City Home", "Number of goals conceded", "Number of Matches", [221, 223]))
 obj.append(PlotObject(teams["ManUnitedAway"], "FTAG", "Man Utd Away", "Number of goals scored", "Number of Matches", [222, 224]))
@@ -152,9 +145,8 @@ plotstats(obj)
 
 
 # ## Simulation
-
-# To simulate the future matches, we can use the poisson distribution to randomly generate future scores and see how the teams stack up against eachother.
-
+# To simulate the future matches, we can use the poisson distribution to randomly generate future scores and see
+# how the teams stack up against eachother.
 def sim_poisson(nums, mean):
     gen = np.random.poisson(lam = mean, size = nums)
     return gen
@@ -163,7 +155,6 @@ def sim_poisson(nums, mean):
 # And then we can use this function to generate some scores for the following cases:
 # Man Utd Home vs Man City Away
 # Man City Home vs Man Utd Away
-
 def generate_scores():
     gen_scores = []
     gen_scores.append(sim_poisson(1000, teams["ManUnitedHome"]["FTHG"].mean()))
@@ -176,12 +167,13 @@ def generate_scores():
     
     return {"ManUtdHvsMC": MUHVSMCA, "ManCityHvsMU": MCHVSMUA}
 
+# Then we generate these scores to get a dictionary of paired data.
 genscores = generate_scores()
 
 
 # We can summarise this data by looking at who won each game.
 
-
+# Helper function to check for the winner given a result which would always be in the format (x,y) as per genscores.
 def getresult(arr):
     if arr[0] > arr[1]:
         return "H"
@@ -190,11 +182,11 @@ def getresult(arr):
     else:
         return "D"
 
-
+# Get a summary of wins
 ManUtdHvsMC = [getresult(i) for i in (genscores["ManUtdHvsMC"])]
-
 ManCityHvsMU = [getresult(i) for i in (genscores["ManCityHvsMU"])]
 
+# Split the simulated results into home and away stats.
 home_stats = {
     "MU Win": Counter(ManUtdHvsMC)["H"],
     "MC Win":  Counter(ManCityHvsMU)["H"],
@@ -205,17 +197,20 @@ away_stats = {
     "MC Win": Counter(ManUtdHvsMC)["A"],
 }
 
-
+# Handling draws appropriately as they will just be considered as one group regardless of what side they were playing on.
 draws = Counter(ManCityHvsMU)["D"] + Counter(ManUtdHvsMC)["D"]
 
+# Get a summary of the data that has been modelled.
 df = pd.DataFrame(home_stats, index=["Home"])
 df = df.append(pd.DataFrame(away_stats, index=["Away"]))
 print("Total number of simulations: ",len(ManUtdHvsMC)+len(ManCityHvsMU))
 print("\nWins/Losses simulated: \n",df)
 print("\nTotal draws in simulation: ",Counter(ManUtdHvsMC)["D"] + Counter(ManCityHvsMU)["D"])
 
-
+# Here we will be able to find probabilities of each team home and away and see who has a higher chance of winning on a certain side.
 def findoverallprob(team):
+    assert isinstance(team, str), 'Argument of wrong type!'
+
     total_matches = len(ManUtdHvsMC) + len(ManCityHvsMU)
     if team == "MU":
         team_win = home_stats["MU Win"] + away_stats["MU Win"]
@@ -223,6 +218,8 @@ def findoverallprob(team):
         team_win = home_stats["MC Win"] + away_stats["MC Win"]
     elif team == "D":
         team_win = draws
+    else:
+        raise ValueError('Team must be either MU or MC.')
     return team_win/float(total_matches)
 
 
@@ -251,47 +248,53 @@ def findprob(team, side):
         elif team == "MC":
             total_matches = away_stats["MC Win"] + home_stats["MU Win"]
             return away_stats["MC Win"]/float(total_matches)
+    else:
+        raise ValueError('Side must be home or away')
+
+    raise ValueError('Team must be either MU or MC.')
+
 
 
 # And we can see the following probabilities from this:
-
 print("Probability of MU win given they play home: ", findprob("MU", "home"))
 print("Probability of MC win given they play home: ", findprob("MC", "home"))
 print("Probability of MU win given they play away: ", findprob("MU", "away"))
 print("Probability of MC win given they play away: ", findprob("MC", "away"))
 
 
-# From this we can observe that Manchester City has a higher chance of winning both playing home or away compared to Manchester United. From this we can confidently predict that in a match up of Manchester United vs Manchester City in the Premier League, based on the historical data; Manchester City will win the game. 
+# To verify our model we can pick another two teams at random and pair them up against eachother to see if our simulation model is a feasible
+# and reliable choice for predicting football scores. We will choose Chelsea and Arsenal for this test.
 
-
-# Generate 10 games between Chelsea and Arsenal
-
+# Get existing games played by both teams
 chelsea = [data.query("HomeTeam == 'Chelsea'"), data.query("AwayTeam == 'Chelsea'")]
 arsenal = [data.query("HomeTeam == 'Arsenal'"), data.query("AwayTeam == 'Arsenal'")]
 
+# Describe the data to see how the teams have performed at home
 print(chelsea[0].describe())
 print(arsenal[0].describe())
 
+# And away
 print(chelsea[1].describe())
 print(arsenal[1].describe())
 
+# Get the actual result between the teams to compare with our simulation
 CHAW = data.query("AwayTeam == 'Arsenal' & HomeTeam == 'Chelsea'")
 AHCW = data.query("AwayTeam == 'Chelsea' & HomeTeam == 'Arsenal'")
-# print("Chelsea home vs Arsenal Away\n", CHAW)
-# print("Arsenal Home vs Chelsea Away\n", AHCA)
 
+# Generate random games of Chelsea home and away
 g_CH = sim_poisson(10, chelsea[0]["FTHG"].mean())
 g_CA = sim_poisson(10, chelsea[1]["FTAG"].mean())
 
+# Generate random games of Arsenal home and away=
 g_AH = sim_poisson(10, arsenal[0]["FTHG"].mean())
 g_AA = sim_poisson(10, arsenal[1]["FTAG"].mean())
 
 
-#g_AHCW = sim_poisson(10, arsenal["FTHG"].mean())
-
+# Combine these results as paired data.
 CHAA = zip(g_CH, g_AA)
 AHCA = zip(g_AH, g_CA)
 
+# And print out our findings to see what happened.
 print("Chelsea Home vs Arsenal Away: \n")
 print("Actual score: ", CHAW)
 print("Simulated match results (C = chelsea win, A = Arsenal win, D = Draw): ", ["C" if getresult(x) == "H" else "A" if getresult(x) == "A" else "D" for x in CHAA])
@@ -301,4 +304,3 @@ print("\n-------------------------\n")
 print("Arsenal Home vs Chelsea Away: \n")
 print("Actual score: ", AHCW)
 print("Simulated match results (C = chelsea win, A = Arsenal win, D = Draw): ", ["C" if getresult(x) == "H" else "A" if getresult(x) == "A" else "D" for x in AHCA])
-
