b17a23c19bd38e6cec54a01e22f05b3ea09c051d
Ihtasham <redrailz@gmail.com>
2018-03-23 18:48:54 +0000

    finalised report

--- CW2/solution.py
+++ CW2/solution.py
@@ -4,7 +4,6 @@ import scipy.stats as stats
 import math
 import numpy as np
 import matplotlib.mlab as mlab
-import ast
 from sklearn import linear_model, preprocessing, cross_validation
 from sklearn.cross_validation import cross_val_score
 from sklearn import feature_selection, metrics
@@ -12,8 +11,17 @@ from sklearn.cluster import KMeans
 from sklearn import datasets 
 from sklearn.decomposition import PCA
 
-data = pd.read_csv("diabetic_data.csv")
+data = pd.read_csv("diabetic_data.csv") # Firstly read the data-set csv
 
+'''
+Cleaning the data:
+- Converting ? or spsace ' ' values to NaN
+- Checking for missing values in the dataframe
+- Mapping the IDs given in the ID csv to the IDs we are interested in
+- Converting the predicting column to YES NO
+- Dropping rows if more than 75% of values are missing from a column to eliminate columns that may not help us in the future
+- Dropping rows with more than 40 missing columns, total being 50
+'''
 def cleanData(df):
 	df.replace(regex=r'\?', value=np.nan, inplace=True) # Convert ? to NaN 
 	pd.isnull(df).values.sum() # Check for missing values
@@ -29,6 +37,8 @@ def cleanData(df):
 	df = df.dropna(axis=0, thresh=40)	
 	return df
 
+
+# Normalise function to normalise the data
 def normalize(df):
     assert isinstance(df, pd.DataFrame), 'Argument of wrong type!'
     num_cols = df.select_dtypes(include=[np.number]).copy()
@@ -36,6 +46,10 @@ def normalize(df):
     df_norm = df_norm.dropna()
     return df_norm
 
+
+'''
+Function to drop list of columns from a given dataframe df
+'''
 def dropAll(df, todrop):
 	for i in todrop:
 		try:
@@ -50,7 +64,12 @@ def filterCols(dataframe, cols):
 data = cleanData(data)
 print(data)
 
-
+'''
+Function for plotting bloxplots
+df - dataframe to plot
+plot_cols - columns to plot 
+by - Column(Y axis) to plot against
+'''
 def boxplot(df, plot_cols, by):
 	for col in plot_cols:
 		fig = plt.figure(figsize=(6, 5))
@@ -61,8 +80,10 @@ def boxplot(df, plot_cols, by):
 		ax.set_ylabel(col)
 		plt.show()
 
+# Plotting the numerical columns we are interested in
 boxplot(data, ["number_emergency", "num_medications", "num_procedures", "number_diagnoses"], "readmitted")
 
+# Making a copy of the dataframe to remove outliers and plot again to see if it helps.
 xdata = data.copy()
 xdata["readmitted"] = xdata["readmitted"].map({"NO": 0, "YES": 1})
 xdata = xdata.select_dtypes(include=[np.number])
@@ -71,21 +92,26 @@ xdata = xdata[(np.abs(stats.zscore(xdata)) < 4).all(axis=1)]
 
 print("outlier removed count: ", len(xdata))
 
+# Replot the data after normalising it
 xdata = normalize(xdata)
 boxplot(xdata, ["number_emergency", "num_medications", "num_procedures", "number_diagnoses"], "readmitted")
 
-
+# Create a new column for normalised data, dropping the ids and other cols we are not interested in
 df_norm = normalize(data)
-
 df_norm = dropAll(df_norm, ["encounter_id", "patient_nbr", "discharge_disposition_id", "admission_source_id"])
-
 print(df_norm.columns)
 
+# Filter the columns that are relavent and we want to analyse further
 filtered = filterCols(data, ["race", "gender", "age", "time_in_hospital", "num_procedures", "number_diagnoses", "readmitted", "admission_type_id"])
 
 print(filtered.describe())
 # print("Skewness of num_procedures: ", stats.normaltest(filtered["num_procedures"]))
 
+'''
+Function to plot pie charts
+dct - data groups to plot 
+xlabel - label of pie chart
+'''
 def createPieChart(dct, xlabel):
 	labels = list(dct.keys())
 	sizes  = [dct[x] for x in labels]
@@ -96,6 +122,16 @@ def createPieChart(dct, xlabel):
 	plt.xlabel(xlabel)
 	plt.show()
 
+
+'''
+Function to plot a bar chart
+df - dataframe to plot
+cols - columns from the dataframe to plot
+xcol - x axis column
+ycol - y axis column
+xlabel - x axis label
+ylabel - y axis label
+'''
 def createBar(df, cols, xcol, ycol, xlabel, ylabel):
 	ser = df.value_counts().to_frame().reset_index().sort_values(by="index")
 	ser.columns = cols
@@ -104,7 +140,7 @@ def createBar(df, cols, xcol, ycol, xlabel, ylabel):
 	plt.ylabel(ylabel)
 	plt.show()
 
-
+# Get the groups from the dataframe to create pie charts for checking proportionality
 race = filtered.race.value_counts()
 #createPieChart(race, "Diabetic patients by race")
 
@@ -135,6 +171,7 @@ print(grp2)
 
 #createBar(filtered.number_diagnoses, ["number diagnoses", "number of patients"], 0, 1, "Number of Diagnoses", "Number of patients")
 
+# Pairing the data to create a scatter matrix to see correlations between different numeric columns
 def pairs(plot_cols, df):
 	from pandas.tools.plotting import scatter_matrix
 	fig = plt.figure(1, figsize=(12, 12))
@@ -149,6 +186,13 @@ def pairs(plot_cols, df):
 #plot_cols = ["time_in_hospital", "num_lab_procedures", "num_procedures", "num_medications", "number_diagnoses"]
 #pairs(plot_cols, data)
 
+'''
+Function to plot clusters
+df1 - x axis dataframe to plot
+df2 - y axis dataframe to plot 
+xlabel - x axis label
+ylabel - y axis label
+'''
 def plotclusters(df1, df2, xlabel, ylabel):
 	plt.plot(df1, df2,'ro')
 	plt.title(xlabel+" vs "+ylabel)
@@ -163,41 +207,54 @@ plotclusters(data["number_outpatient"], data["readmitted"], "# Outpatient", "Rea
 plotclusters(data["number_emergency"], data["readmitted"], "# Emergency", "Readmitted")
 plotclusters(data["number_inpatient"], data["readmitted"], "# Inpatient", "Readmitted")
 
+
+# Create a new dataframe for building a predictive model
 _df = data.copy()
 
+# Convert the readmitted column to 0,1 NO,YES respectively
 _df["readmitted"] = _df["readmitted"].map({"NO": 0, "YES": 1})
 
+# Remove all the outliers, using the same technique as before
 _df = _df[(np.abs(stats.zscore(_df.select_dtypes(exclude='object'))) < 3).all(axis=1)]
 
+# Using a logistic regression model to create a model based on the features we are interested in
 clf = linear_model.LogisticRegression()
+
+# List of columns to consider - numerical
 f_cols = ['number_emergency', 'time_in_hospital', 'num_lab_procedures', 'num_procedures', 'num_medications', 'number_diagnoses', 'number_inpatient', 'number_outpatient']
 X = _df[f_cols]
 Y = _df['readmitted']
 clf.fit(X, Y)
+
+# Obtaining the score for the model, and coefficients
 print("Model score:\n {}".format(clf.score(X,Y)))
 print("Intercept:\n {}".format(clf.intercept_))
 print("Coefficients:\n")
 for feat, coef in zip(f_cols, clf.coef_[0]):
 	print(" {:>20}: {}".format(feat, coef))	
 
+# Validate the model with test and training set to see how it performs
 X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size=0.2)
 clf = linear_model.LogisticRegression()
 clf.fit(X_train, Y_train)
 print("Score against training data: {}".format(clf.score(X_train, Y_train)))
 print("Score against test data: {}".format(clf.score(X_test, Y_test)))
 
+# List of categorical columns we are interested in based on our analysis
 cat_cols = ["age", "admission_type_id", "diabetesMed", "change", "insulin"]
 t_df = _df[f_cols+cat_cols]
 df_cat = pd.get_dummies(t_df, columns=cat_cols)
 
+# Running the logistic regression again to pick features that we can use to build up our model further
 model = linear_model.LogisticRegression()
 X0 = df_cat.ix[:, df_cat.columns != 'readmitted']
-Y0 = _df['readmitted']
+Y0 = _df['readmitted']	
 selector = feature_selection.RFE(model, n_features_to_select = 20, step=1)
 selector = selector.fit(X0, Y0)
 selected_features = df_cat.ix[:, selector.support_]
 print("Selected features:\n{}".format(',\n'.join(list(selected_features))))
 
+# Use the selected freatures to rerun the model and see if that wil help for improvement and revalidate this against the test set
 X = selected_features
 Y = _df['readmitted']
 trainX, testX, trainY, testY = cross_validation.train_test_split(X, Y, test_size=0.3, random_state=0)
@@ -210,6 +267,7 @@ scores = cross_validation.cross_val_score(
 linear_model.LogisticRegression(), X, Y, scoring='accuracy', cv=8)
 print("Cross validation mean scores: {}".format(scores.mean()))
 
+# Creating an ROC curve to get a better sense of the linear model
 prob = np.array(clf.predict_proba(testX)[:, 1])
 testY += 1
 fpr, sensitivity, _ = metrics.roc_curve(testY, prob, pos_label=2)
